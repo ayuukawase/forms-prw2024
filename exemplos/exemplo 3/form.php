4d9d493c6f403e5d4198251b85a9f2db3345000b
ayuukawase <108367413+ayuukawase@users.noreply.github.com>
2024-06-18 12:00:50 -0300

    form.php

--- exemplos/exemplo 3/form.php
+++ exemplos/exemplo 3/form.php
@@ -8,15 +8,15 @@
 </head>
 <body>
     <?php
-        $velocidade = $_POST['velocidade'];
-        $distancia = $_POST['distancia'];
-        $consumo = $_POST['consumo'];
+        $velocidade = floatval($_POST['velocidade']);
+        $distancia = floatval($_POST['distancia']);
+        $consumo = floatval($_POST['consumo']);
 
         $horas = $distancia / $velocidade;
         $combustivel = $distancia / $consumo;
 
-        echo "Total horas: ".number_format(round($horas, PHP_ROUND_HALF_UP), 2,".","")."<br>";
-        echo "Total combustivel: ".number_format(round($combustivel, PHP_ROUND_HALF_UP), 2,".","")."<br>";
+        echo "Total horas: ".number_format($horas, 2,".","")."<br>";
+        echo "Total combustivel: ".number_format($combustivel, 2,".","")."<br>";
     ?>
 </body>
 </html>
